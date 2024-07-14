3220f6d7d67deff9262e1847904f23206d7675d8
Samir ali <alisamir8565@gmail.com>
2024-07-14 07:43:20 +0530

    Perform Operator operations in the file

--- JavaScriptChallenges/02_Operators/Operators.js
+++ JavaScriptChallenges/02_Operators/Operators.js
@@ -1,6 +1,52 @@
-let a = 45
-let b = 30
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+//1. Arithmetic Operations
+    let a = 5
+    let b = 30
+    console.log("Sum is:",a+b);
+    console.log("Subtraction value is:",a-b);
+    console.log("multiply sum is:",a*b);
+    console.log("Divide is:",a/b);
+    console.log("Reminder is:",a%b);
+
+//2. Assignment Operations
+    console.log("Divid:",a/=b);
+    console.log("Addition is:",a+=b);
+    console.log("Subtration:",a-=b);
+    console.log("Multiply:",a*=b);
+   
+//3. Camparision Operations
+    if(a<b){
+        console.log("A less than B");
+    } 
+    else if(a>b){
+        console.log("A greather than B");
+    }
+    else if(a<=b){
+        console.log("A less than or equat to B");
+    } 
+    else if(a>=b){
+        console.log("A grater than or equat to B");
+    } 
+    else if(a==b){
+        console.log("A equat to B");
+    }
+    else if(a===b){
+        console.log("A equat to B with same data type");
+    }
+
+//3\4. Logical Operations
+
+let c = 10
+let d = 15
+if(a<b && c<d){
+    console.log("Both condition is true");
+}
+else if(b>a || c<d){
+    console.log("One or more condition is true");
+}
+if(c!=b){
+    console.log("C not equal to b");
+}
+
+// 5. Ternary Operations
+const e = c<d? "yes": "No";
+console.log(e);
